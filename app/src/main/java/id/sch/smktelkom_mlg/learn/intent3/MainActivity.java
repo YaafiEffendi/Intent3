6b2c62246d1227bec3907b3b012c27eb77cd029f
GituAja <Gituaja.copyright@Gmail.com>
2016-11-05 10:36:22 +0700

    intent Telepon

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,7 +1,10 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.imageViewPhone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                dialPhoneNumber("08992257999");
+            }
+        });
+    }
+
+    private void dialPhoneNumber(String PhoneNumber)
+    {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel :"+ PhoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 }
